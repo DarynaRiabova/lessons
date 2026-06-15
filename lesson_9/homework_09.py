023f199c275cfbe7f4dcd97c0ab0c621206ddeb0
Daryna Riabova <riabovadaryna@gmail.com>
2026-06-15 17:49:29 +0200

    fix

--- lesson_9/homework_09.py
+++ lesson_9/homework_09.py
@@ -4,6 +4,10 @@ class Rhomb:
             if value <= 0:
                 raise ValueError("side_a must be greater than 0")
 
+        if name == "corner_a":
+            if not (0 < value < 180):
+                raise ValueError("corner_a must be between 0 and 180")
+
             self.__dict__[name] = value
             self.__dict__["corner_b"] = 180 - value
             return
