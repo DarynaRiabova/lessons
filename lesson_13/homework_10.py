8e8df05f4427dffaf3d31f47328b0cd20bf7b5e2
Daryna Riabova <riabovadaryna@gmail.com>
2026-06-24 22:42:30 +0200

    add commit

--- lesson_13/homework_10.py
+++ lesson_13/homework_10.py
@@ -39,7 +39,7 @@ def log_event(username: str, status: str):
         logger.error(log_message)
 
 
-# tests - перший варіант
+# tests перший варіант, теоретично можна покращити та прибрати дублювання коду
 
 
 class TestLog(unittest.TestCase):
